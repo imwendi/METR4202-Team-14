e8b838ff08a9bbed9b8bfc3cac884cef4a9fee1d
imwendi <54469385+imwendi@users.noreply.github.com>
2022-09-22 12:20:59 +1000

    setup project python module structure

---
+++ scripts/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from kinematics import test_lib
+
+if __name__ == '__main__':
+    test_lib.test_func()
+    
---
+++ src/kinematics/test_lib.py
@@ -0,0 +1,2 @@
+def test_func():
+    print("test_func from test_lib ran!")
