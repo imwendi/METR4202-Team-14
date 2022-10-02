9395af9343adee7f04232bd6fa27c4ddd1620578
Wendi Ma <mawendi851@gmail.com>
2022-10-02 10:22:09 +1000

    update slider_actual with joint angle option selection

--- src/kinematics/plotting/slider_actual.py
+++ src/kinematics/plotting/slider_actual.py
@@ -19,13 +19,13 @@ ax_y = plt.axes([0.25, 0.15, 0.65, 0.03])
 ax_z = plt.axes([0.25, 0.1, 0.65, 0.03])
 ax_phi = plt.axes([0.25, 0.05, 0.65, 0.03])
 
-lim = L
-
+lim = L + base_height
 x_slide = Slider(ax_x, 'End-affector x', -lim, lim, 0)
 y_slide = Slider(ax_y, 'End-affector y', -lim, lim, 0)
-z_slide = Slider(ax_z, 'End-affector z', L*2/3, lim, lim/2)
-phi_slide = Slider(ax_phi, 'orientation', -np.pi, np.pi, 0)
+z_slide = Slider(ax_z, 'End-affector z', 0, lim, lim*2/3)
+phi_slide = Slider(ax_phi, 'orientation', -180, 180, 0)
 
+JOINT_ANGLES_IDX = 3
 possible_joint_angles = np.zeros((4, 1))
 
 
@@ -34,10 +34,19 @@ def update(val):
     x = x_slide.val
     y = y_slide.val
     z = z_slide.val
-    phi = phi_slide.val
+    phi = np.deg2rad(phi_slide.val)
 
-    joint_angles = rk.ik(np.array([x, y, z]), phi)[:, 3]
-    #print(joint_angles)
+    possible_joint_angles = rk.ik(np.array([x, y, z]), phi)
+    # uncomment to print options!!
+    # for i in range(4):
+    #     joint_angles = possible_joint_angles[:, i]
+    #     pos = np.around(rk.joint_pos(joint_angles), 3)
+    #     deg_angles = np.around(np.rad2deg((joint_angles)), 3)
+    #
+    #     print(f"Option {i}\nPos:{pos}\nJoint Angles:{deg_angles}\n")
+    # print('_'*80)
+
+    joint_angles = possible_joint_angles[:, JOINT_ANGLES_IDX]
     joint_pos = rk.joint_pos(joint_angles)
 
     # get collision
@@ -47,7 +56,6 @@ def update(val):
         print("\n")
 
     ax.clear()
-    lim = L
     ax.set_xlim([-lim, lim])
     ax.set_ylim([-lim, lim])
     ax.set_zlim([-lim, lim])
@@ -57,17 +65,29 @@ def update(val):
     last_plot = plot_4r_robot(joint_pos, ax, surface_range=(-lim, lim))
 
 
+# joint angle idx button
+# Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
+joint_angle_idx_ax = fig.add_axes([0.4, 0.01, 0.1, 0.04])
+button1 = Button(joint_angle_idx_ax, str(JOINT_ANGLES_IDX), hovercolor='0.975')
+def change_joint_angle_idx(event):
+    global JOINT_ANGLES_IDX
+    JOINT_ANGLES_IDX = (JOINT_ANGLES_IDX + 1) % 4
+    button1.label.set_text(str(JOINT_ANGLES_IDX))
+    update(None)
+button1.on_clicked(change_joint_angle_idx)
+
+
 # slider reset button
 # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='0.975')
+resetax = fig.add_axes([0.8, 0.01, 0.1, 0.04])
+button2 = Button(resetax, 'Reset', hovercolor='0.975')
 def reset(event):
     pass
     x_slide.reset()
     y_slide.reset()
     z_slide.reset()
     phi_slide.reset()
-button.on_clicked(reset)
+button2.on_clicked(reset)
 
 
 if __name__ == '__main__':
