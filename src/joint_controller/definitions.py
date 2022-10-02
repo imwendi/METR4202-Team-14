ec3a259d47dc01fba9bbe7d44eb6a17488efd8ae
imwendi <54469385+imwendi@users.noreply.github.com>
2022-10-02 15:52:26 +1000

    update definitions.py with dynamixel multipliers

--- src/joint_controller/definitions.py
+++ src/joint_controller/definitions.py
@@ -4,12 +4,14 @@ Various constants / default values
 '''
 import numpy as np
 
+# ___________________________________ ROS ______________________________________
 # common node names
 NODE_JOINT_STATES = 'joint_states'
 NODE_DESIRED_JOINT_STATES = 'desired_joint_states'
 NODE_DESIRED_POS = 'desired_pose'
 NODE_DESIRED_POSE4 = 'desired_pose4'
 
+# _________________________________ GEOMETRY ___________________________________
 # robot measurements [mm] (426 total)
 BASE_HEIGHT = 46
 L1 = 55
@@ -19,7 +21,10 @@ L4 = 110
 LINK_LENGTHS = np.array([L1, L2, L3, L4])
 L = np.sum(LINK_LENGTHS)
 
-# motion
+# ________________________________ DYNAMIXEL ___________________________________
+# Dynamixel 4 angle needs to be *= -1 as it is installed in opposite orientation
+# to Dynamixels 2 and 3
+DIRECTIONAL_MULTIPLIERS = np.array([1, 1, 1, -1])
 DEFAULT_VELOCITY = 0.5 # [rad/s]
 
 
