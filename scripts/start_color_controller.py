b087821e8be3eb600eb9524365a34cfe13f27d16
imwendi <54469385+imwendi@users.noreply.github.com>
2022-10-20 22:01:46 +1000

    update launch file

--- scripts/start_color_controller.py
+++ scripts/start_color_controller.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+import rospy
 from vision.color_controller import ColorNode, CAMERA_ID
 
+rospy.init_node('color_node')
+colornode = ColorNode(CAMERA_ID)
+
 if __name__ == '__main__':
-    rospy.init_node('color_node')
-    colornode = ColorNode(CAMERA_ID)
     rospy.spin()
