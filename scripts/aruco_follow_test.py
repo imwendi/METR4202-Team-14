5c4a30ba22f04533ffe6097411b178ac11d51ec3
imwendi <54469385+imwendi@users.noreply.github.com>
2022-10-20 11:14:31 +1000

    add cube class

--- scripts/aruco_follow_test.py
+++ scripts/aruco_follow_test.py
@@ -4,42 +4,78 @@ import rospy
 import numpy as np
 from joint_controller.definitions import *
 from geometry_msgs.msg import Pose, Point, Quaternion
+from claw.claw_controller import ClawController
+from kinematics.kinematics_base import KinematicsBase
+from kinematics.utils import rot
 
-pose_pub = rospy.Publisher(NODE_DESIRED_POS, Pose, queue_size=10)
-transform_matrix = np.array([[0, 1, 0, 190],
-                            [1, 0, 0, 0],
-                            [0, 0, -1, 400],
-                            [0, 0, 0, 1]])
+
 
 from fiducial_msgs.msg import Fiducial
 from fiducial_msgs.msg import FiducialTransformArray
+from sensor_msgs.msg import JointState
+from team14.msg import Pose4
 
-def print_fiducials(fid: FiducialTransformArray):
-    if len(fid.transforms) > 0:
-        scale = 450/2.42
-        point = np.array([fid.transforms[0].transform.translation.x, fid.transforms[0].transform.translation.y, fid.transforms[0].transform.translation.z])
-        point = scale*point
-        point = np.append(point, 1)
-        new_p = transform_matrix@point
-        if fid!= None:
-            rospy.loginfo(f'x:  {fid.transforms[0].transform.translation.x*scale}')
-            rospy.loginfo(f'y:  {fid.transforms[0].transform.translation.y*scale}')
-            rospy.loginfo(f'z:  {fid.transforms[0].transform.translation.z*scale}')
-        print('x: ', new_p[0], 'y: ', new_p[1], 'z: ', new_p[2], "\n")
-        new_p = new_p[:-1]
 
-        msg = Pose(Point(*new_p), Quaternion(0, 0, 0, 0))
-        pose_pub.publish(msg)
+class ArucoFollow():
+
+    def __init__(self):
+
+        self.pose_pub = rospy.Publisher(NODE_DESIRED_POS, Pose, queue_size=10)
+        
+        self.sub = rospy.Subscriber( '/fiducial_transforms', # Topic name
+                                FiducialTransformArray, # Message type
+                                self.print_fiducials # Callback function (required)
+        )
+        self.pose_sub = rospy.Subscriber('current_pose',
+                                    Pose4,
+                                    self._pose_sub_handler)
+
+        self.claw = ClawController()
+        self.transform_matrix = np.array([[0, 1, 0, 190],
+                                        [1, 0, 0, 0],
+                                        [0, 0, -1, 426+25],
+                                        [0, 0, 0, 1]])
+        self.current_pos = np.zeros(3)
+
+
+    def print_fiducials(self, fid: FiducialTransformArray):
+        if len(fid.transforms) > 0:
+            scale = 450/2.42
+            point = np.array([fid.transforms[0].transform.translation.x, fid.transforms[0].transform.translation.y, fid.transforms[0].transform.translation.z])
+            point = scale*point
+            point = np.append(point, 1)
+            new_p = self.transform_matrix@point
+            # if fid!= None:
+            #     rospy.loginfo(f'x:  {fid.transforms[0].transform.translation.x*scale}')
+            #     rospy.loginfo(f'y:  {fid.transforms[0].transform.translation.y*scale}')
+            #     rospy.loginfo(f'z:  {fid.transforms[0].transform.translation.z*scale}')
+            print('x: ', new_p[0], 'y: ', new_p[1], 'z: ', new_p[2], "\n")
+            new_p = new_p[:-1]
+
+            self.claw.set('open')
+            msg = Pose(Point(*new_p), Quaternion(0, 0, 0, 0))
+            self.pose_pub.publish(msg)
+            print('current_pos: ', self.current_pos)
+            #print('new_p: ', new_p)
+            print('shape: ',self.current_pos.shape)
+
+            print(np.linalg.norm(self.current_pos - new_p))
+            if np.linalg.norm(self.current_pos - new_p) < 10:
+
+                self.claw.set('grip')
+
+
         #rospy.loginfo(f'fid:  {fid.transforms[0].transform}')
+    def _pose_sub_handler(self, T: Pose4):
+        self.current_pos = np.array(T.position)
+        #self.current_pos = rot(0, 0, np.pi)@np.array(T.position)
+        #print(self.current_pos)
+
+
 
 def main():
 
-    # Create subscriber
-    sub = rospy.Subscriber(
-        '/fiducial_transforms', # Topic name
-        FiducialTransformArray, # Message type
-        print_fiducials # Callback function (required)
-    )
+    af = ArucoFollow()
 
     # Initialise node with any node name
     rospy.init_node('aruco_follow_node')
---
+++ src/arcuo_vision/cube.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import rospy
+import numpy as np
+from scipy.spatial.transform import Rotation
+from joint_controller.definitions import *
+from geometry_msgs.msg import Transform
+
+
+class Cube:
+    def __init__(self,
+                 cache_length=50,
+                 avg_length=10,
+                 moving_threshold=10):
+        """
+        Constructor
+
+        Args:
+            cache_length: length of position and z orientation caches
+            avg_length: number of values to compute moving average over
+            moving_threshold: position delta threshold to determine if cube is moving
+        """
+        # cube positions
+        self.positions = []
+        # cube z orientations
+        self.orientations = []
+        self.cache_length = cache_length
+        self.avg_length = avg_length
+        self.moving_threshold = moving_threshold
+        self.moving = False
+
+    def update(self, transform: Transform):
+        if transform is None:
+            return
+
+        # extract
+        position = np.array(transform.translation)
+        quaternion = np.array(transform.rotation)
+        rot = Rotation.from_quat(quaternion)
+        z_orientation = rot.as_euler('xyz')[-1]
+
+        # check if moving
+        pos_change = position - self.average_pos()
+        if np.linalg.norm(pos_change) > self.moving_threshold:
+            self.moving = True
+        else:
+            self.moving = False
+
+        self.positions.append(position)
+        self.orientations.append(z_orientation)
+        # truncate lists if needed
+        if (len(self.positions) > self.cache_length):
+            self.positions = self.positions[-self.cache_length:]
+        if (len(self.orientations) > self.cache_length):
+            self.orientations = self.orientations[-self.cache_length:]
+
+    def average_pos(self):
+        """
+        Returns:
+            Moving average over last self.avg_pos values
+        """
+        values = np.array(self.positions[:max(-self.avg_length, len(self.positions))])
+
+        # TODO: check axis!
+        return np.mean(values, axis=-1)
+
+
+
+# class Cube():
+#     """
+#     Class for saving detcted aruco cubes
+#
+#     """
+#     def __init__(self,
+#                 id: int,
+#                 curr_pos: np.array,
+#                 curr_rot: np.array):
+#         """
+#         Constructor
+#
+#         """
+#         self.id = id
+#         self.curr_pos = curr_pos
+#         self.curr_rot = curr_rot
+#         self.prev_pos = None
+#         #self.prev_rot = None
+#
+#     def is_moving(self):
+#         if self.prev_pos == None:
+#             #??
+#             return True
+#
+#         pos_err = 0.5
+#         rot_err = 0.5
+#
+#         # Check if position has changed
+#         if (abs(curr_pos[0] - prev_pos[0]) < pos_err) and \
+#            (abs(curr_pos[1] - prev_pos[1]) < pos_err) and \
+#            (abs(curr_pos[2] - prev_pos[2]) < pos_err):
+#            return False
+#         else:
+#             return True
